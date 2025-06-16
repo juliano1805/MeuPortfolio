cfabaaf4ff6cfe64138832a80fd6588f56ed6ce0
Juliano Matheus Ferreira <julianomatheusferreira@gmail.com>
2025-06-16 00:20:20 -0300

    Ajusta caminho de importação para usar caminho relativo

--- src/components/ArticlesSection.tsx
+++ src/components/ArticlesSection.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
-import { getLatestArticles } from '@/lib/articles.ts';
+import { getLatestArticles } from '../lib/articles';
 import { ArrowRight, Calendar } from 'lucide-react';
 
 const ArticlesSection = () => {
